cbb75f4324d6be96975e311df02cadbbe5c9ab5b
dutchmeat <dutchmeat_@hotmail.com>
2014-12-25 21:55:08 +0100

    Removed my original django-unity-env environment

--- Django/DjangoUnityTutorial/django-unity-env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!E:\Workspace\Unity\SomeUnityDjangoProject\Django\DjangoUnityTutorial\DjangoUnityTutorial\django-unity-env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
