7eb667d23e09cc8c48d822110f95b2f07ad7528c
dutchmeat <dutchmeat_@hotmail.com>
2014-12-26 12:29:04 +0100

    Added an extension method for JToken because I dislike out methods

---
+++ Unity/Assets/ExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+public static class ExtensionMethods {
+    public static JToken GetValue(this JObject jobject, string propertyName) {
+        JToken obj;
+        if (jobject.TryGetValue(propertyName, out obj)) {
+            return obj;
+        }
+        return null;
+    }
+
+}
