810c7e24c559ea549a02e2d1f2c0f12a1130266f
dutchmeat <dutchmeat_@hotmail.com>
2014-12-30 23:06:15 +0100

    Added  updated field to the score model, make sure you syncdb

--- Django/DjangoUnityTutorial/unitybackendapp/models.py
+++ Django/DjangoUnityTutorial/unitybackendapp/models.py
@@ -6,6 +6,7 @@ class Score(models.Model):
     name = models.CharField(max_length=20)
     score = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return '%s - %d' % (self.name, self.score)
