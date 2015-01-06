fb488347be9ca8b85d268c9e6fe4738a4cdd7918
unknown <eamon@CHICKENPC32.littlechicken.local>
2015-01-06 15:13:24 +0100

    Added a simple block spawning and pushing mechanism

---
+++ Unity/Assets/Scripts/Block.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name.Equals("Ground")) {
+            Destroy(gameObject, 1f);
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockSpawner : MonoBehaviour {
+    public GameObject BlockPrefab;
+    void Awake() {
+        if (BlockPrefab == null) {
+            Debug.LogWarning("BlockPrefab == null");
+            enabled = false;
+        }
+    }
+    private Vector3 GetPosition() {
+        Vector3 position = transform.position;
+        BoxCollider bc = (BoxCollider)collider;
+        float width = bc.bounds.max.x - bc.bounds.min.x;
+        position.x = (bc.bounds.min.x) + (width * (Input.mousePosition.x / Screen.width));
+        return position;
+    }
+	void Update () {
+        if (Input.GetMouseButtonUp(0)) {
+            Vector3 worldPos = GetPosition();
+            Instantiate(BlockPrefab, worldPos, transform.rotation);
+        }
+        if (Input.GetMouseButtonUp(1)) {
+            Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000f) && hit.transform.name.Contains("Block")) {
+                Rigidbody rigidbody = hit.transform.GetComponent<Rigidbody>();
+                if (rigidbody != null) {
+                    rigidbody.AddForce(Vector3.forward * 100, ForceMode.Impulse);
+                }
+            }
+        }
+	}
+}
