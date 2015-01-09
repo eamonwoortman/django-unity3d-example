ffe6571430518178202e5ba4202d7811cf84bafb
eamon woortman <eamon.woortman@gmail.com>
2015-01-09 10:49:09 +0100

    Started with the savegame menu

--- Unity/Assets/Scripts/ExampleProject/GameMenu.cs
+++ Unity/Assets/Scripts/ExampleProject/GameMenu.cs
@@ -3,26 +3,53 @@ using System.Collections;
 
 public class GameMenu : MonoBehaviour {
     public GUISkin Skin;
+
+    public delegate void LoadSaveButtonPressed(string filename);
     public LoadSaveButtonPressed OnSaveButtonPressed;
     public LoadSaveButtonPressed OnLoadButtonPressed;
 
     private Rect windowRect = new Rect(10, 10, 200, 200);
-    private delegate void LoadSaveButtonPressed(string filename);
-    
+    private const string NoSavegamesFound = "No savegames found";
+    private string[] savegameNames = new string[] { NoSavegamesFound };
+    private int selectedNameIndex = -1;
+    private string saveName = "";
+
     public bool InRect(Vector3 mousePosition) {
         return windowRect.Contains(mousePosition);
     }
 
     private void ShowWindow(int id) {
+        GUILayout.BeginVertical();
+        GUILayout.Label("Save games");
+        bool savegamesFound = (savegameNames[0] != NoSavegamesFound);
+        GUI.enabled = savegamesFound;
+        selectedNameIndex = GUILayout.SelectionGrid(selectedNameIndex, savegameNames, 1);
+        GUI.enabled = true;
+        GUILayout.Space(100);
+
+        saveName = GUILayout.TextField(saveName);
         GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Push me")) {
+
+        GUI.enabled = (saveName != "");
+        if (GUILayout.Button("Save")) {
+            if (OnSaveButtonPressed != null) {
+                OnSaveButtonPressed(savegameNames[selectedNameIndex]);
+            }
         }
-        GUILayout.Label("THIS IS A LABEL");
-        GUILayout.Toggle(true, "TOGGLE");
+
+        GUI.enabled = savegamesFound;
+        if (GUILayout.Button("Load")) {
+            if (OnLoadButtonPressed != null) {
+                OnLoadButtonPressed(savegameNames[selectedNameIndex]);
+            }
+        }
+        GUI.enabled = true;
+
         GUILayout.EndHorizontal();
+        GUILayout.EndVertical();
     }
     private void OnGUI() {
         GUI.skin = Skin;
-        windowRect = GUILayout.Window(0, windowRect, ShowWindow, "superwindow");
+        windowRect = GUILayout.Window(0, windowRect, ShowWindow, "Load/save game");
     }
 }
