01968b1865cdf0b9798b7c7f46dae6410f1bcce7
Bas Roding <bas.roding@gmail.com>
2015-01-13 10:16:59 +0100

    Added Score class

---
+++ Unity/Assets/Scripts/Score.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Score {
+    public int Id;
+    public int Amount;
+    public DateTime Created;
+}
