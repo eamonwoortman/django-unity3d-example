2003b65f879f5dbd2e6ec6016d081135c377180d
Bas Roding <bas.roding@gmail.com>
2015-01-13 12:12:26 +0100

    EnterNameMenu is now the HighscoreMenu and can be used to show the highscores

---
+++ Unity/Assets/Scripts/GUI/HighscoreMenu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HighscoreMenu : BaseMenu {
+
+    public Action OnCancel;
+    public bool Loading;
+
+    public List<Score> Scores {
+        get {
+            return scores;
+        }
+        set {
+            scores = value.OrderBy(s => s.Amount).ThenBy(s => s.Updated).Reverse().ToList();
+        }
+    }
+
+    private List<Score> scores;
+
+    public HighscoreMenu() {
+        windowRect = new Rect(Screen.width  / 2 - 100, Screen.height / 2 - 100, 200, 200);
+    }
+    
+    private void ShowWindow(int id) {
+
+        if (Loading) {
+            GUILayout.Label("Loading..");
+        } else {
+            foreach (Score score in scores) {
+                GUILayout.Label(score.Updated.ToShortDateString() + " - " + score.Amount.ToString());
+            }
+            if (GUILayout.Button("OK")) {
+                if (OnCancel != null) {
+                    OnCancel();
+                }
+            }
+        }
+    }
+
+    private void OnGUI() {
+        GUI.skin = Skin;
+        windowRect = GUILayout.Window(0, windowRect, ShowWindow, "Highscore");
+    }
+	
+}
