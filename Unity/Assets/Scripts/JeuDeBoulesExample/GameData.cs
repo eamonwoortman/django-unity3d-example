f0977cc95a0332c2a2f768c133e7a5c16eaa5ba2
eamon woortman <eamon.woortman@gmail.com>
2015-01-14 09:22:10 +0100

    Added gametype in GameData and implemented basegame

--- Unity/Assets/Scripts/JeuDeBoulesExample/GameData.cs
+++ Unity/Assets/Scripts/JeuDeBoulesExample/GameData.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 
 public class GameData {
+    public enum Gametypes {
+        JeuDeBoule,
+        Blockgame
+    }
+    public Gametypes Gametype;
     public int Turn = 0;
     public float Score = 0;
 }
